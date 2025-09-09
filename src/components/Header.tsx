2235660e6c5a2ed5b8bb5809b2cff45ee53ea193
Markuss Auseklis <markussauseklis02@gmail.com>
2025-09-09 16:33:20 +0300

    Added initial Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,102 @@
 import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import { Link } from "react-router-dom";
+
+const pages = ["Home", "About", "Gallery"];
 
 export default function Header(){
+
+    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+    );
+
+    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
     return(
-        <div></div>
+        <AppBar position="static">
+            <Container>
+                <Toolbar>
+                    {/*Logo*/}
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{
+                        mr: 2,
+                        display: { xs: "flex", md: "flex" },
+                        fontWeight: 700,
+                        letterSpacing: ".2rem",
+                        color: "inherit",
+                        textDecoration: "none",
+                        }}>
+                        Codnity Task
+                    </Typography>
+
+                    {/* Mobile */}
+                    <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" }, justifyContent: "flex-end" }}>
+                        <IconButton
+                            size="large"
+                            aria-label="menu"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit">
+                            <MenuIcon />
+                        </IconButton>
+                        
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: "bottom",
+                                horizontal: "right",
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: "top",
+                                horizontal: "right",
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{ display: { xs: "block", md: "none" } }}
+                            >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu} component={Link} to={`/${page.toLowerCase()}`}>
+                                <Typography textAlign="center">{page}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+
+                    {/*Desktop */}
+                    <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" }, justifyContent: "flex-end" }}>
+                        {pages.map((page) => (
+                        <Button
+                            key={page}
+                            component={Link}
+                            to={`/${page.toLowerCase()}`}
+                            sx={{ my: 2, color: "white", display: "block" }}>
+                            {page}
+                        </Button>
+                        ))}
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
     )
 }
